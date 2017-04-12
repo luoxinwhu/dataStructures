f49ae92065d45d6c86626767d9d9ece425dc0975
Luoxin <359437435@qq.com>
2017-04-12 15:57:03 +0800

    Stack应用——进制转换

--- Stack/Stack/main.cpp
+++ Stack/Stack/main.cpp
@@ -1,40 +1,19 @@
-ï»¿//
-//  main.cpp
-//  dataStructure
-//
-//  Created by mac on 2017/4/11.
-//  Copyright Â© 2017å¹´ mac. All rights reserved.
-//
-
-
 #include <iostream>
+#include <cmath>
 #include "Stack.h"
-#include "Coordinate.h"
+#include "radixTrans.h"
+//#include "Coordinate.h"
 using namespace std;
-
-#define ISFROMBUTTOM true
 
 int main(void)
 {
-	//å®ä¾‹åŒ–Stackç±»æ¨¡æ¿
-	Stack<Coordinate> *pStack = new Stack<Coordinate>(10);
-	Coordinate coor1(1,2), coor2(3,4);
-	
-	pStack->push(coor1);
-	pStack->push(coor2);
-	pStack->stackTraverse(ISFROMBUTTOM);
+	//ÊµÀı»¯StackÀàÄ£°å
+	Stack<int> *pStack = new Stack<int>(10);
+	int N;
 
-	/*if (pStack->pop(coor1))
-	{
-		cout << coor1 << endl;
-	}
+	cin >> N;
+	transformation(N, HEXADECIMAL, pStack);
 
-	pStack->clearStack();
-	if (pStack->pop(coor1))
-	{
-		cout << coor1 << endl;
-	}
-	*/
 	delete pStack;
 	pStack = NULL;
 	system("pause");
---
+++ Stack/Stack/radixTrans.h
@@ -0,0 +1,37 @@
+﻿/*
+* radixTrans.h
+* 栈应用——进制转换
+* 描述： 输入任意十进制整数N， 输出二进制、八进制、16进制的数
+* 公式： N = （N div d) *d + N mod d
+*/
+
+#include "Stack.h"
+
+#define ISFROMBUTTOM true
+#define ISFROMTOP	 false
+#define BINARY		2
+#define OCTONARY	8
+#define HEXADECIMAL 16
+
+template <typename T>
+void transformation(int N, int radix, Stack<T> *pStack)
+{
+	char model[] = "0123456789ABCDEF";
+	int mod = 0;
+	int elem = 0;
+	while (N != 0)
+	{
+		mod = N % radix;
+		if (!pStack->push(mod))
+			break;
+		N = N / HEXADECIMAL;
+	}
+
+	cout << HEXADECIMAL << "进制数为：";
+	while (!pStack->StackEmpty())
+	{
+		pStack->pop(elem);
+		cout << model[elem];
+	}
+	cout << endl;
+}
